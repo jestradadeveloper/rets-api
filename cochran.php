9716098c0ff75d3bf046facda9ef3ecb663ad53d
jomaestradama <jomaestradama@ittepic.edu.mx>
2019-05-16 22:05:09 -0500

    CA

--- cochran.php
+++ cochran.php
@@ -84,7 +84,7 @@
 		//getCochranListing($allRecords, 'condis',10,'condos');
 		//getCochranListing($queryCochranProperties, 'cochranfeatured',10,'allrecords');
 		//getCochranListing($queryCochranProperties, 'cochranfeatured',20,'allrecords');
-		getCochranListing($allRecords, 'fullrecords-A',3000,'allrecords');
+		getCochranListing($allRecords, 'retlists',4000,'all');
 function getCochranListing($query, $file, $limit, $type){
 	$listingCustom = [];
 	date_default_timezone_set('America/Mexico_City');
@@ -108,22 +108,94 @@ function getCochranListing($query, $file, $limit, $type){
 	// make the first request
    
         $connect = $rets->Login();
+			//condos
+			if($type=="all"){
 			
-			$resultsA = $rets->Search('Property', 'A', $query, ['Limit' => $limit, 'Count' => 1, 'Select' =>  'LIST_5,LIST_8,LIST_3,LIST_31,LIST_35,LIST_34,LIST_29,LIST_40,LIST_46,LIST_47,LIST_39,LIST_22,LIST_88,LIST_78,LIST_130,LIST_48,LIST_66,LIST_67,LIST_87']);			
+			$resultsA = $rets->Search('Property', 'A', $query, ['Limit' => $limit, 'Count' => 1, 'Select' =>  'LIST_5,LIST_8,LIST_3,LIST_31,LIST_35,LIST_34,LIST_29,LIST_40,LIST_46,LIST_47,LIST_39,LIST_22,LIST_88,LIST_78,LIST_130,LIST_48,LIST_66,LIST_67,LIST_87']);
+			
 			//Houses	
 			$resultsB = $rets->Search('Property', 'B', $query, ['Limit' => $limit, 'Count' => 1, 'Select' => 'LIST_5,LIST_8,LIST_3,LIST_31,LIST_35,LIST_34,LIST_29,LIST_40,LIST_46,LIST_47,LIST_39,LIST_22,LIST_88,LIST_78,LIST_130,LIST_48,LIST_66,LIST_67,LIST_87']);
+			
 			//land
-			$resultsE = $rets->Search('Property', 'E', $query, ['Limit' => $limit, 'Count' => 1, 'Select' => 'LIST_5,LIST_8,LIST_3,LIST_31,LIST_35,LIST_34,LIST_29,LIST_40,LIST_46,LIST_47,LIST_39,LIST_22,LIST_88,LIST_78,LIST_130,LIST_57,LIST_87']);			
+			$resultsE = $rets->Search('Property', 'E', $query, ['Limit' => $limit, 'Count' => 1, 'Select' => 'LIST_5,LIST_8,LIST_3,LIST_31,LIST_35,LIST_34,LIST_29,LIST_40,LIST_46,LIST_47,LIST_39,LIST_22,LIST_88,LIST_78,LIST_130,LIST_57,LIST_87']);
+			
 			//Commercial
 			$resultsF = $rets->Search('Property', 'F', $query, ['Limit' => $limit, 'Count' => 1, 'Select' =>  'LIST_5,LIST_8,LIST_3,LIST_31,LIST_35,LIST_34,LIST_29,LIST_40,LIST_46,LIST_47,LIST_39,LIST_22,LIST_88,LIST_78,LIST_130,LIST_48,LIST_69,LIST_57,LIST_87']);
+			
+			//Business
+			$resultsG = $rets->Search('Property', 'G', $query, ['Limit' => $limit, 'Count' => 1, 'Select' =>  'LIST_5,LIST_8,LIST_3,LIST_31,LIST_35,LIST_34,LIST_29,LIST_40,LIST_46,LIST_47,LIST_39,LIST_22,LIST_88,LIST_78,LIST_130,LIST_48,LIST_57,LIST_69,LIST_87']);
+			
+			//Fractional
+			$resultsH = $rets->Search('Property', 'H', $query, ['Limit' => $limit, 'Count' => 1, 'Select' => 'LIST_5,LIST_8,LIST_3,LIST_31,LIST_35,LIST_34,LIST_29,LIST_40,LIST_46,LIST_47,LIST_39,LIST_22,LIST_88,LIST_78,LIST_130,LIST_48,LIST_57,LIST_69,LIST_87']);
+			
+			//Multifamily
+			$resultsI = $rets->Search('Property', 'I', $query, ['Limit' => $limit, 'Count' => 1, 'Select' => 'LIST_5,LIST_8,LIST_3,LIST_31,LIST_35,LIST_34,LIST_29,LIST_40,LIST_46,LIST_47,LIST_39,LIST_22,LIST_88,LIST_78,LIST_130,LIST_48,LIST_57,LIST_87']);
+			
 			$v = $resultsB->toArray();
 			$v2 = $resultsA->toArray();
 			$v3 = $resultsF->toArray();
+			$v4 = $resultsG->toArray();
+			$v5 = $resultsH->toArray();
+			$v6 = $resultsI->toArray();
 			$v7 = $resultsE->toArray();
+
 			$resultado = array_merge($v, $v2);
 			$resultado = array_merge($resultado, $v3);
+			$resultado = array_merge($resultado, $v4);
+			$resultado = array_merge($resultado, $v5);
+			$resultado = array_merge($resultado, $v6);
 			$resultado = array_merge($resultado, $v7);
 			
+			
+		    }else if($type=="condos"){
+				$results = $rets->Search('Property', 'A',$query, ['Limit' => $limit, 'Count' => 1, 'Select' =>  'LIST_5,LIST_1,LIST_8,LIST_3,LIST_31,LIST_35,LIST_34,LIST_29,LIST_40,LIST_46,LIST_47,LIST_39,LIST_22,LIST_88,LIST_78,LIST_130,LIST_48,LIST_66,LIST_67,LIST_149,GF20110906150955980018000000,LIST_148,LIST_87']);
+				$resultado=$results->toArray();
+			}else if($type=="commercial"){
+				$results = $rets->Search('Property', 'F', $query, ['Limit' => $limit, 'Count' => 1, 'Select' =>  'LIST_5,LIST_1,LIST_8,LIST_3,LIST_31,LIST_35,LIST_34,LIST_29,LIST_40,LIST_46,LIST_47,LIST_39,LIST_22,LIST_88,LIST_78,LIST_130,LIST_48,LIST_69,LIST_57,GF20110906151348774098000000,LIST_87']);
+				$resultado=$results->toArray();
+			}else if($type=="land"){
+				$results = $rets->Search('Property', 'E', $query, ['Limit' => $limit, 'Count' => 1, 'Select' => 'LIST_5,LIST_1,LIST_8,LIST_3,LIST_31,LIST_35,LIST_34,LIST_29,LIST_40,LIST_46,LIST_47,LIST_39,LIST_22,LIST_88,LIST_78,LIST_130,LIST_57,GF20110906151235936884000000,LIST_87']);
+				$resultado=$results->toArray();
+			}else if($type=="houses"){
+				$results = $rets->Search('Property', 'B', $query, ['Limit' => $limit, 'Count' => 1, 'Select' =>  'LIST_5,LIST_1,LIST_8,LIST_3,LIST_31,LIST_35,LIST_34,LIST_29,LIST_40,LIST_46,LIST_47,LIST_39,LIST_22,LIST_88,LIST_78,LIST_130,LIST_48,LIST_66,LIST_67,LIST_149,GF20110906151054800976000000,LIST_87']);
+				$resultado=$results->toArray();
+			}else{
+				$resultsA = $rets->Search('Property', 'A', $query, ['Limit' => $limit, 'Count' => 1, 'Select' =>  'LIST_5,LIST_1,LIST_8,LIST_3,LIST_31,LIST_35,LIST_34,LIST_29,LIST_40,LIST_46,LIST_47,LIST_39,LIST_22,LIST_88,LIST_78,LIST_130,LIST_48,LIST_66,LIST_67,LIST_149,GF20110906150955980018000000,LIST_148,LIST_36,LIST_87']);
+			
+				//Houses	
+				$resultsB = $rets->Search('Property', 'B', $query, ['Limit' => $limit, 'Count' => 1, 'Select' => 'LIST_5,LIST_1,LIST_8,LIST_3,LIST_31,LIST_35,LIST_34,LIST_29,LIST_40,LIST_46,LIST_47,LIST_39,LIST_22,LIST_88,LIST_78,LIST_130,LIST_48,LIST_66,LIST_67,LIST_149,GF20110906151054800976000000,LIST_36,LIST_87']);
+				
+				//land
+				$resultsE = $rets->Search('Property', 'E', $query, ['Limit' => $limit, 'Count' => 1, 'Select' => 'LIST_5,LIST_1,LIST_8,LIST_3,LIST_31,LIST_35,LIST_34,LIST_29,LIST_40,LIST_46,LIST_47,LIST_39,LIST_22,LIST_88,LIST_78,LIST_130,LIST_57,GF20110906151235936884000000,LIST_87']);
+				
+				//Commercial
+				$resultsF = $rets->Search('Property', 'F', $query, ['Limit' => $limit, 'Count' => 1, 'Select' =>  'LIST_5,LIST_1,LIST_8,LIST_3,LIST_31,LIST_35,LIST_34,LIST_29,LIST_40,LIST_46,LIST_47,LIST_39,LIST_22,LIST_88,LIST_78,LIST_130,LIST_48,LIST_69,LIST_57,GF20110906151348774098000000,LIST_36,LIST_87']);
+				
+				//Business
+				$resultsG = $rets->Search('Property', 'G', $query, ['Limit' => $limit, 'Count' => 1, 'Select' =>  'LIST_5,LIST_1,LIST_8,LIST_3,LIST_31,LIST_35,LIST_34,LIST_29,LIST_40,LIST_46,LIST_47,LIST_39,LIST_22,LIST_88,LIST_78,LIST_130,LIST_48,LIST_57,LIST_69,GF20110906151708515998000000,LIST_36,LIST_87']);
+				
+				//Fractional
+				$resultsH = $rets->Search('Property', 'H', $query, ['Limit' => $limit, 'Count' => 1, 'Select' => 'LIST_5,LIST_1,LIST_8,LIST_3,LIST_31,LIST_35,LIST_34,LIST_29,LIST_40,LIST_46,LIST_47,LIST_39,LIST_22,LIST_88,LIST_78,LIST_130,LIST_48,LIST_57,LIST_69,GF20101117190906920838000000,LIST_36,LIST_149,LIST_87']);
+				
+				//Multifamily
+				$resultsI = $rets->Search('Property', 'I', $query, ['Limit' => $limit, 'Count' => 1, 'Select' => 'LIST_5,LIST_1,LIST_8,LIST_3,LIST_31,LIST_35,LIST_34,LIST_29,LIST_40,LIST_46,LIST_47,LIST_39,LIST_22,LIST_88,LIST_78,LIST_130,LIST_48,LIST_57,GF20110906152000926926000000,LIST_87']);
+				
+				$v = $resultsB->toArray();
+				$v2 = $resultsA->toArray();
+				$v3 = $resultsF->toArray();
+				$v4 = $resultsG->toArray();
+				$v5 = $resultsH->toArray();
+				$v6 = $resultsI->toArray();
+				$v7 = $resultsE->toArray();
+	
+				$resultado = array_merge($v, $v2);
+				$resultado = array_merge($resultado, $v3);
+				$resultado = array_merge($resultado, $v4);
+				$resultado = array_merge($resultado, $v5);
+				$resultado = array_merge($resultado, $v6);
+				$resultado = array_merge($resultado, $v7);
+
+			}
 	
 			foreach ($resultado as $r) {
 					if(isset($r['LIST_66'])){
@@ -135,7 +207,7 @@ function getCochranListing($query, $file, $limit, $type){
 						$bedrooms = "1";
 
 					}
-					if($r['LIST_8']=='F'){
+					if($r['LIST_8']=='F' || $r['LIST_8']=='G' || $r['LIST_8']=='H'){
 						$bathrooms = $r['LIST_69'];
 					}else{
 						$bathrooms = "0";
@@ -185,6 +257,50 @@ function getCochranListing($query, $file, $limit, $type){
 						$street_number= ''.$r['LIST_31'];
 						$street_name=$r['LIST_34'];
 					}
+					if($r['LIST_8']=='G'){
+		
+						$beachfront = $r['GF20110906151708515998000000'];
+						$pet = "No";
+						$precontruction = "No";
+						if(!empty($r['LIST_36'])){
+							$unit_number= ''.$r['LIST_36'];
+						}else{
+							$unit_number= '';
+						}
+						$street_number= ''.$r['LIST_31'];
+						$street_name=$r['LIST_34'];
+					}
+					if($r['LIST_8']=='H'){
+						$pet =$r['LIST_149'];
+						$beachfront = $r['GF20101117190906920838000000'];
+						$precontruction = "No";
+						if(!empty($r['LIST_36'])){
+							$unit_number= ''.$r['LIST_36'];
+						}else{
+							$unit_number= '';
+						}
+						$street_number= ''.$r['LIST_31'];
+						$street_name=$r['LIST_34'];
+					}
+					if($r['LIST_8']=='I'){
+		
+						$beachfront = $r['GF20110906152000926926000000'];
+						$pet = "No";
+						$precontruction = "No";
+						if(!empty($r['LIST_36'])){
+							$unit_number= ''.$r['LIST_36'];
+						}else{
+							$unit_number= '';
+						}
+						$street_number= ''.$r['LIST_31'];
+						$street_name= $r['LIST_34'];
+					}
+					
+					
+					//$pet = isset($r['LIST_149']) ? $r['LIST_149'] : false;
+
+					
+
 					if($r['LIST_8']=='E'){
 						$size=   $r['LIST_57'];
 					}else{
@@ -195,6 +311,9 @@ function getCochranListing($query, $file, $limit, $type){
 					$url= $photos[0]->getLocation(); //obtiene la primera imagen de cada listado
 					$thumbnail = $rets->GetObject("Property", "Thumbnail",$r['LIST_3'], "*", 1); //especifica el tamaño de la foto a obtener
 					$url_thumbnail= $thumbnail[0]->getLocation(); 
+					//$supplemental = $rets->GetObject("Property", "Supplement",$r['LIST_1'], "*", 1); 
+				
+					//$log->addInfo("Suplementals-".$supplementals);
 					$listingCustom[] = array(
 						'id' => $r['LIST_3'],
 						'type'=>$r['LIST_8'],
