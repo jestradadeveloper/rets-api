e6630167d552e1f9d4f59fe0b8446720c6688bc1
Jose Estrada <webdeveloper@vgloreto.com>
2019-06-11 02:32:29 -0500

    ip

--- index.php
+++ index.php
@@ -7,8 +7,9 @@
  * @package  realhomes
  * @subpackage modern
  */
-$data = file_get_contents ('data/retslist.json');
-$json = json_encode($data,JSON_PRETTY_PRINT);
+//$data = file_get_contents ('data/retslist.json');
+$dataip =file_get_contents('http://67.205.141.118/');
+$json = json_encode($dataip,JSON_PRETTY_PRINT);
 header('Content-Type: application/json');
-echo $data;
+echo $dataip;
 
